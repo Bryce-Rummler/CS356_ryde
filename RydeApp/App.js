bd319419dbf43c8759bb44f9b15df7404ad04f94
Bryce <rummler.b@gmail.com>
2022-03-29 01:02:25 -0600

    Added basic bottom sheet

--- RydeApp/App.js
+++ RydeApp/App.js
@@ -1,20 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button } from "react-native";
+import Animated from "react-native-reanimated";
+import BottomSheet from "reanimated-bottom-sheet";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+  const renderContent = () => (
+    <View
+      style={{
+        backgroundColor: "white",
+        padding: 10,
+        height: 450,
+        alignItems: "center",
+      }}
+    >
+      <View
+        style={{
+          borderBottomColor: "grey",
+          borderBottomWidth: 3,
+          width: 100,
+          marginBottom: 10,
+        }}
+      />
+      <Text>Swipe up for more info about bus route</Text>
     </View>
+  );
+
+  const sheetRef = React.useRef(null);
+
+  return (
+    <>
+      <View style={styles.container}>
+        <Text>Map component should go here</Text>
+        <StatusBar style="auto" />
+      </View>
+      <BottomSheet
+        ref={sheetRef}
+        snapPoints={[450, 200]}
+        initialSnap={1}
+        borderRadius={10}
+        renderContent={renderContent}
+      />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "lightblue",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
