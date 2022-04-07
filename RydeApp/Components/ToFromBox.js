a5633fc3de8a3752033b30ea38add594ae8e986f
Bryce <rummler.b@gmail.com>
2022-04-06 21:33:58 -0600

    searchbar has fields to fill boxes with

--- RydeApp/Components/ToFromBox.js
+++ RydeApp/Components/ToFromBox.js
@@ -1,13 +1,113 @@
+import moment from "moment";
 import React from "react";
-import { StyleSheet, Text, View, Button, Image, TextInput } from "react-native";
-import { FlipInEasyX } from "react-native-reanimated";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  Image,
+  TextInput,
+  Pressable,
+  ScrollView,
+} from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { FadeIn, FlipInEasyX } from "react-native-reanimated";
+import customData from "../dummydata/data.json";
+
+function getRoutes() {
+  // const routes = JSON.parse(customData);
+  const routes = customData.routes;
+  // console.log(routes);
+  let routeList = [];
+  for (const route of routes) {
+    routeList.push({
+      key: route.key,
+      route: route.route,
+      stop: route.stop,
+      color: route.color,
+    });
+  }
+  return routeList;
+}
 
 export default function ToFromBox() {
   const [from, onChangeFrom] = React.useState("Brigham Young University");
   const [to, onChangeTo] = React.useState("Branbury");
+  const [searchSug, setSearchSug] = React.useState(false);
+  const [fromBox, setFromBox] = React.useState(true);
+
+  setSearchSugTrue = (fromOrTo) => {
+    if (fromOrTo === "from") {
+      setFromBox(true);
+    } else {
+      setFromBox(false);
+    }
+    setSearchSug(true);
+  };
+
+  setSearchSugFalse = () => {
+    setSearchSug(false);
+  };
+
+  fillBox = (stop) => {
+    if (fromBox) {
+      onChangeFrom(stop);
+    } else {
+      onChangeTo(stop);
+    }
+  };
+
+  function ColoredCircle(props) {
+    const colorStyle = {
+      backgroundColor: props.color,
+    };
+    return <View style={[styles.coloredCircle, colorStyle]}></View>;
+  }
+
+  function SearchList() {
+    const routes = getRoutes();
+    const listItems = routes.map((route) => (
+      <TouchableOpacity
+        key={route.key}
+        onPress={() => {
+          fillBox(route.stop);
+        }}
+      >
+        <View style={styles.searchItem}>
+          <View>
+            <Text style={styles.stop}>{route.stop}</Text>
+          </View>
+          <View style={styles.routeContainer}>
+            <ColoredCircle color={route.color} />
+            <Text style={styles.route}>{route.route}</Text>
+          </View>
+        </View>
+      </TouchableOpacity>
+    ));
+    return <View>{listItems}</View>;
+  }
+
+  function SearchSugBox() {
+    const display = searchSug;
+    if (display) {
+      return (
+        <View style={styles.searchBoxContainer}>
+          <ScrollView style={styles.searchBoxContent}>
+            {/* <Text>Display</Text>
+            <Text>{fromBox ? "from" : "to"}</Text>
+            <Text>{moment("07:50", "HH:mm").format()}</Text> */}
+            <SearchList />
+          </ScrollView>
+        </View>
+      );
+    } else {
+      return <View></View>;
+    }
+  }
 
   return (
     <>
+      <SearchSugBox />
       <View style={styles.container}>
         <View>
           <Image
@@ -16,19 +116,43 @@ export default function ToFromBox() {
           />
         </View>
         <View>
+          {/* <Pressable
+            onPress={() => {
+              this.setSearchSugTrue("from");
+            }}
+          > */}
           <Text style={styles.textLable}>From</Text>
           <TextInput
             style={styles.input}
             onChangeText={onChangeFrom}
+            onFocus={() => {
+              this.setSearchSugTrue("from");
+            }}
+            onBlur={() => {
+              this.setSearchSugFalse();
+            }}
             value={from}
           ></TextInput>
+          {/* </Pressable> */}
           <View style={styles.line}></View>
+          {/* <Pressable
+            onPress={() => {
+              this.setSearchSugTrue("to");
+            }}
+          > */}
           <Text style={styles.textLable}>To</Text>
           <TextInput
             style={styles.input}
             onChangeText={onChangeTo}
+            onFocus={() => {
+              this.setSearchSugTrue("to");
+            }}
+            onBlur={() => {
+              this.setSearchSugFalse();
+            }}
             value={to}
           ></TextInput>
+          {/* </Pressable> */}
         </View>
       </View>
     </>
@@ -36,6 +160,54 @@ export default function ToFromBox() {
 }
 
 const styles = StyleSheet.create({
+  searchBoxContainer: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "contain",
+    backgroundColor: "white",
+    zIndex: 800,
+  },
+  searchBoxContent: {
+    position: "absolute",
+    top: 200,
+    width: 275,
+    height: 260,
+    alignSelf: "center",
+    // backgroundColor: "lightgray",
+    overflow: "scroll",
+    borderTopColor: "gray",
+    borderTopWidth: 1,
+    borderBottomColor: "gray",
+    borderBottomWidth: 1,
+    paddingRight: 8,
+  },
+  searchItem: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginTop: 10,
+    paddingBottom: 10,
+    borderBottomColor: "gray",
+    borderBottomWidth: 1,
+  },
+  stop: {
+    fontSize: 16,
+  },
+  routeContainer: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  route: {
+    fontSize: 12,
+  },
+  coloredCircle: {
+    borderRadius: 3,
+    height: 6,
+    width: 6,
+    margin: 5,
+  },
   container: {
     position: "absolute",
     top: 40,
@@ -49,6 +221,12 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     alignItems: "center",
     alignSelf: "center",
+    zIndex: 900,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.8,
+    shadowRadius: 1,
+    elevation: 5,
   },
   tofromicon: {
     height: 68,
